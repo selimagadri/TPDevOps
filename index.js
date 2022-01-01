fdfa2425f85ca0e3ed516974ad7837dfff749079
selima gadri <62613548+selimagadri@users.noreply.github.com>
2022-01-01 17:47:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,12 +13,3 @@ function getWeekNumber(d) {
 }
 
 module.exports = (date) => getWeekNumber(date || new Date());
-
-const express = require("express");
-const app = express();
-
-app.get("/", function (req, res) {
-  res.send("Hello World!!!");
-});
-
-app.listen(process.env.PORT || 5000);
